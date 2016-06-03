1b6fe9b9025143a77c5694ff9750dbdcffcea469
the_input <chjalmar@gmail.com>
2016-06-03 19:56:33 -0400

    init

---
+++ persona.js
@@ -0,0 +1,16 @@
+﻿function Persona(nombre) {
+    this.nombre = nombre;
+    this.presentarse = function() { console.log("Hola soy " + this.nombre); }
+}
+
+function Empleado(nombre, trabajo) {
+    this.nombre = nombre;
+    this.trabajo = trabajo;
+    this.presentarse = function() { console.log("Hola soy " + this.nombre + " y trabajo en " + this.trabajo); }
+}
+
+var jose = new Persona('Jose');
+jose.presentarse(); // imprime: Hola soy Jose
+
+var robert = new Empleado('Robert', 'csw');
+robert.presentarse(); // imprime: Hola soy Robert y trabajo en csw
